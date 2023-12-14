19ec4ece6d24a5e5bd16ebd3448a0c6b1181882b
Gerardo Urzua <80187497+geUrz@users.noreply.github.com>
2023-12-14 13:02:03 -0800

    add env2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const ENV = {
   /* SERVER_HOST: 'http://localhost:1337',
   API_URL: 'http://localhost:1337/api', */
-  SERVER_HOST: 'http://vocal-coach-strapi.onrender.com',
-  API_URL: 'http://vocal-coach-strapi.onrender.com/api',
+  SERVER_HOST: 'https://vocal-coach-strapi.onrender.com',
+  API_URL: 'https://vocal-coach-strapi.onrender.com/api',
   ENDPOINTS: {
     AUTH: {
       REGISTER: 'auth/local/register',
